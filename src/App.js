1af54264b5e7f266c27bf139d31d8ee4aea7ccae
Md Arwaz <mdarwaznew2017@gmail.com>
2024-01-08 21:46:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import WeatherApp from "./Components/WeatherApp/WeatherApp.jsx";
+
+
+const App = () => {
+  return (
+    <div>
+      <WeatherApp />
+    </div>
+  );
+};
+
+export default App;
